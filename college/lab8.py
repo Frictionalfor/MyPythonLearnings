a415a184815dced593dd90c3ed5e1f446a52646d
Frictionalfor <swanand2008.08@gmail.com>
2026-03-17 09:14:44 +0530

    changes of practical

--- college/lab8.py
+++ college/lab8.py
@@ -1,30 +1,14 @@
-"""Packages and Modules"""
 
-#Build-in Modules
-import qrcode
+import math
+print("Square root of 16:", math.sqrt(16))
+print("Value of pi:", math.pi)
 
-data = "https://www.geeksforgeeks.org/python/python-programming-language-tutorial/"
-qr = qrcode.QRCode(
-    version=None,
-    error_correction=qrcode.constants.ERROR_CORRECT_H,
-    box_size=10,
-    border=4,
-)
 
-qr.add_data(data)
-qr.make(fit=True)
-
-img = qr.make_image(fill_color="black", back_color="white")
-img.save("lab8(.py)output.png")
-print("QR code generated and saved as lab8(.py)output.png")
-
-#Importing a user-defined module
 import mymodule
 
-mymodule.greet("Happy")
 result = mymodule.add(5, 3)
 print("Addition result:", result)
-squared = mymodule.square(4)
-print("Square result:", squared)
+difference = mymodule.subtract(5, 3)
+print("Subtraction result:", difference)
 
 
--- college/mymodule.py
+++ college/mymodule.py
@@ -1,10 +1,7 @@
 #This is a simple module for demonstration purposes.
 
-def greet(name):
-    print("Hello", name)
-
 def add(a, b):
     return a + b
 
-def square(n):
-    return n * n
+def subtract(a, b):
+    return a - b
